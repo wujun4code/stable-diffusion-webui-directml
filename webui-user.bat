e90b80ad391fc8df67e32218a40de4bbf17c8d48
Wu Jun <wujun4code@gmail.com>
2023-03-22 19:50:55 +0800

    feat: local.

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,7 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\admin\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS=--precision full --no-half --opt-sub-quad-attention --disable-nan-check --lowvram  --disable-opt-split-attention --api
 call webui.bat
